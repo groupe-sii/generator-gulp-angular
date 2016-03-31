9dbb982dbfec49771739f115a2e09e2ced6f9925
Valentin GOT <vgot@sii.fr>
2016-03-31 13:19:19 +0200

    fix(angular-translate): add angular config

--- app/templates/src/app/_index.config.js
+++ app/templates/src/app/_index.config.js
@@ -6,9 +6,18 @@
         .config(config);
 
     /** @ngInject */
-    function config($logProvider, toastrConfig) {
-        // Enable log
+    function config($logProvider<% if (props.otherModulesKeys.indexOf('translate') !== -1) { -%>, $translateProvider<% } -%>) {
+
+        // Logging
         $logProvider.debugEnabled(true);
+
+        <% if (props.otherModulesKeys.indexOf('translate') !== -1) { -%>
+        // Translation
+        var language = navigator.browserLanguage || navigator.language;
+        $translateProvider.translations('en', LANGUAGES.en);
+        $translateProvider.translations('fr', LANGUAGES.fr);
+        $translateProvider.preferredLanguage((angular.isDefined(LANGUAGES[language])) ? language : 'en');
+        <% } -%>
     }
 
 })();
