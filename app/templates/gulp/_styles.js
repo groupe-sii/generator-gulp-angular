073c131710f72b9e1ecdd01fc5f17bc8f4a8dd21
Valentin GOT <vgot@sii.fr>
2016-04-01 14:39:52 +0200

    fix(gulp): add styles-reload task

--- app/templates/gulp/_styles.js
+++ app/templates/gulp/_styles.js
@@ -11,26 +11,36 @@ var path = require('path'),
     wiredep = require('wiredep').stream,
     _ = require('lodash');
 
-gulp.task('styles', function () {
-<% if (props.cssPreprocessor.key === 'less') { -%>
-    var lessOptions = {
-        options: [
-            'bower_components',
-        path.join(conf.paths.src, '/app')
-        ]
-    };
-<% } if (props.cssPreprocessor.extension === 'scss') { -%>
-    var sassOptions = {
-        style: 'expanded'
-    };
-<% } -%>
+gulp.task('styles-reload', ['styles'], function() {
+    return buildStyles()
+        .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+    return buildStyles();
+});
+
+var buildStyles = function() {
+    <% if (props.cssPreprocessor.key === 'less') { -%>
+        var lessOptions = {
+            paths: [
+                'bower_components',
+                path.join(conf.paths.src, '/app')
+            ],
+            relativeUrls : true
+        };
+    <% } if (props.cssPreprocessor.extension === 'scss') { -%>
+        var sassOptions = {
+            style: 'expanded'
+        };
+    <% } -%>
 
     var injectFiles = gulp.src([
         path.join(conf.paths.src, '/app/**/*.<%- props.cssPreprocessor.extension %>'),
         path.join('!' + conf.paths.src, '/app/index.<%- props.cssPreprocessor.extension %>'),
-        path.join(conf.paths.src, '/style/**/*.<%- props.cssPreprocessor.extension %>'),
-        path.join('!' + conf.paths.src, '/style/index.<%- props.cssPreprocessor.extension %>')
-    ], {read: false});
+        path.join(conf.paths.src, '/styles/**/*.<%- props.cssPreprocessor.extension %>'),
+        path.join('!' + conf.paths.src, '/styles/index.<%- props.cssPreprocessor.extension %>')
+    ], { read: false });
 
     var injectOptions = {
         transform: function(filePath) {
@@ -42,34 +52,33 @@ gulp.task('styles', function () {
         addRootSlash: false
     };
 
-<% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
-    var cssFilter = $.filter('**/*.css', {restore: true});
-<% } -%>
+    <% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
+        var cssFilter = $.filter('**/*.css', { restore: true });
+    <% } -%>
 
     return gulp.src([
-        path.join(conf.paths.src, '/app/index.<%- props.cssPreprocessor.extension %>'),
-        path.join(conf.paths.src, '/style/index.<%- props.cssPreprocessor.extension %>')
-    ])
-        .pipe($.inject(injectFiles, injectOptions))
-        .pipe(wiredep(_.extend({}, conf.wiredep)))
-<% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
-        .pipe($.rubySass(sassOptions)).on('error', conf.errorHandler('RubySass'))
-        .pipe(cssFilter)
-        .pipe($.sourcemaps.init({loadMaps: true}))
-<% } else { -%>
-        .pipe($.sourcemaps.init())
-<% } if (props.cssPreprocessor.key === 'less') { -%>
-        .pipe($.less(lessOptions)).on('error', conf.errorHandler('Less'))
-<% } else if (props.cssPreprocessor.key === 'node-sass') { -%>
-        .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
-<% } else if (props.cssPreprocessor.key === 'stylus') { -%>
-        .pipe($.stylus()).on('error', conf.errorHandler('Stylus'))
-<% } -%>
-        .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
+                path.join(conf.paths.src, '/app/index.<%- props.cssPreprocessor.extension %>'),
+                path.join(conf.paths.src, '/styles/index.<%- props.cssPreprocessor.extension %>'),
+            ])
+            .pipe($.inject(injectFiles, injectOptions))
+            .pipe(wiredep(_.extend({}, conf.wiredep)))
+        <% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
+    .pipe($.rubySass(sassOptions)).on('error', conf.errorHandler('RubySass'))
+            .pipe(cssFilter)
+            .pipe($.sourcemaps.init({ loadMaps: true }))
+        <% } else { -%>
+    .pipe($.sourcemaps.init())
+        <% } if (props.cssPreprocessor.key === 'less') { -%>
+    .pipe($.less(lessOptions)).on('error', conf.errorHandler('Less'))
+        <% } else if (props.cssPreprocessor.key === 'node-sass') { -%>
+    .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
+        <% } else if (props.cssPreprocessor.key === 'stylus') { -%>
+    .pipe($.stylus()).on('error', conf.errorHandler('Stylus'))
+        <% } -%>
+    .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
         .pipe($.sourcemaps.write())
-<% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
-        .pipe(cssFilter.restore)
-<% } -%>
-        .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')))
-        .pipe(browserSync.reload({stream: true}));
-});
+    <% if (props.cssPreprocessor.key === 'ruby-sass') { -%>
+    .pipe(cssFilter.restore)
+        <% } -%>
+    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')));
+};
